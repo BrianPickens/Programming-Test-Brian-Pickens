9e1e4522523008eae08190d5b43d5f8d057cc2af
Brian Pickens <brianpickens53@gmail.com>
2017-09-19 14:02:34 -0700

    Final Polish Pass
    Added explosion particles and planet animations

--- Programming Test/Assets/Scripts/FoodCatcherScript.cs
+++ Programming Test/Assets/Scripts/FoodCatcherScript.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class FoodCatcherScript : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject _myParticles;
+
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "Vegetable") {
 			GameManagerScript.instance.AddPoints (0);
 		}
 
 		if (other.gameObject.tag == "Meat") {
+			_myParticles.transform.position = other.gameObject.transform.position;
+			_myParticles.SetActive (true);
+			CameraShakeScript.instance.ShakeCamera ();
 			GameManagerScript.instance.AddMeat ();
 		}
 	}
---
+++ Programming Test/Assets/Scripts/MeatParticlesScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeatParticlesScript : MonoBehaviour {
+
+	void OnEnable () {
+		Invoke ("Destroy", 0.45f);
+	}
+
+	private void Destroy () {
+		gameObject.SetActive (false);
+	}
+}
