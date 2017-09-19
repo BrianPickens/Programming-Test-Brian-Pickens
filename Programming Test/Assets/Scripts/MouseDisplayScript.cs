2ba386bface1b0e1da9ccdfef254c8e52772a6b6
Brian Pickens <brianpickens53@gmail.com>
2017-09-19 15:49:47 -0700

    Added Hide Cursor / Game Complete
    Added hide cursor object to scenes. Game is in completed state.

---
+++ Programming Test/Assets/Scripts/MouseDisplayScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Hides cursor during game, but shows it on end screen for credits
+
+public class MouseDisplayScript : MonoBehaviour {
+
+	[SerializeField]
+	private bool _hideCursor;
+
+	void Start () {
+
+		DisplayCursor ();
+
+	}
+
+	private void DisplayCursor () {
+
+		if (_hideCursor) {
+			Cursor.visible = false;
+		} else {
+			Cursor.visible = true;
+		}
+
+	}
+
+}
